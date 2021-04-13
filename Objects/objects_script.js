191213fbd61e02647bafd013572dc6a93803bfa7
sglushkov23 <sglushkov23@gmail.com>
2021-04-14 01:10:37 +0700

    Correct Objects module

--- Objects/objects_script.js
+++ Objects/objects_script.js
@@ -2,21 +2,14 @@
 
 (function () {
     function getCountriesWithMaximumCitiesCount(countries) {
-        var countriesListSortedByCitiesCount = countries.map(function (country) {
-            return {
-                countryName: country.name,
-                citiesCount: country.citiesList.length
-            };
-        }).sort(function (e1, e2) {
-            return e2.citiesCount - e1.citiesCount;
+        var citiesCountsArray = countries.map(function (country) {
+            return country.citiesList.length;
         });
 
-        var maximumCitiesCount = countriesListSortedByCitiesCount[0].citiesCount;
+        var maximumCitiesCount = Math.max.apply(null, citiesCountsArray);
 
-        return countriesListSortedByCitiesCount.filter(function (e) {
-            return e.citiesCount === maximumCitiesCount;
-        }).map(function (e) {
-            return e.countryName;
+        return countries.slice(0).filter(function (e) {
+            return e.citiesList.length === maximumCitiesCount;
         });
     }
 
@@ -32,37 +25,39 @@
         return result;
     }
 
-    var countriesNames = ["Russia", "Germany", "United States of America", "Spain"];
-
-    var citiesNames = [
-        ["Moscow", "Saint Petersburg", "Novosibirsk", "Kazan", "Sochi"],
-        ["Berlin", "Hamburg", "Munich"],
-        ["Miami", "New York", "San Francisco", "Seattle", "Philadelphia"],
-        ["Barcelona", "Valencia", "Malaga"]
-    ];
-
-    var citiesPopulation = [
-        [12678079, 5398064, 1625600, 1143535, 343334],
-        [3769495, 1841179, 1484226],
-        [463347, 8622698, 884363, 608660, 1526006],
-        [1636762, 801456, 574654]
-    ];
-
-    var zippedArrays = countriesNames.map(function (item, i) {
-        return [item, [citiesNames[i], citiesPopulation[i]]];
-    });
-
-    var countries = zippedArrays.map(function (zippedElement) {
-        return {
-            name: zippedElement[0],
-            citiesList: zippedElement[1][0].map(function (e, index) {
-                return {
-                    name: e,
-                    population: zippedElement[1][1][index]
-                };
-            })
-        };
-    });
+    var countries = [{
+        "name": "Russia",
+        "citiesList": [
+            {"name": "Moscow", "population": 12678079},
+            {"name": "Saint Petersburg", "population": 5398064},
+            {"name": "Novosibirsk", "population": 1625600},
+            {"name": "Kazan", "population": 1143535},
+            {"name": "Sochi", "population": 343334}
+        ]
+    }, {
+        "name": "Germany",
+        "citiesList": [
+            {"name": "Berlin", "population": 3769495},
+            {"name": "Hamburg", "population": 1841179},
+            {"name": "Munich", "population": 1484226}
+        ]
+    }, {
+        "name": "United States of America",
+        "citiesList": [
+            {"name": "Miami", "population": 463347},
+            {"name": "New York", "population": 8622698},
+            {"name": "San Francisco", "population": 884363},
+            {"name": "Seattle", "population": 608660},
+            {"name": "Philadelphia", "population": 1526006}
+        ]
+    }, {
+        "name": "Spain",
+        "citiesList": [
+            {"name": "Barcelona", "population": 1636762},
+            {"name": "Valencia", "population": 801456},
+            {"name": "Malaga", "population": 574654}
+        ]
+    }]
 
     console.log("Array of countries objects:");
     console.log(countries);
