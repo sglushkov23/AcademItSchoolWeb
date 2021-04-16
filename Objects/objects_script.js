165805464075c4c3118c9ea110df6e5f7bd7beca
sglushkov23 <sglushkov23@gmail.com>
2021-04-16 23:00:41 +0700

    Correct Objects module

--- Objects/objects_script.js
+++ Objects/objects_script.js
@@ -2,14 +2,14 @@
 
 (function () {
     function getCountriesWithMaximumCitiesCount(countries) {
-        var citiesCountsArray = countries.map(function (country) {
-            return country.citiesList.length;
+        var citiesCounts = countries.map(function (country) {
+            return country.cities.length;
         });
 
-        var maximumCitiesCount = Math.max.apply(null, citiesCountsArray);
+        var maximumCitiesCount = Math.max.apply(null, citiesCounts);
 
-        return countries.slice(0).filter(function (e) {
-            return e.citiesList.length === maximumCitiesCount;
+        return countries.filter(function (e) {
+            return e.cities.length === maximumCitiesCount;
         });
     }
 
@@ -17,7 +17,7 @@
         var result = {};
 
         countries.forEach(function (country) {
-            result[country.name] = country.citiesList.reduce(function (memo, e) {
+            result[country.name] = country.cities.reduce(function (memo, e) {
                 return memo + e.population;
             }, 0);
         })
@@ -26,38 +26,38 @@
     }
 
     var countries = [{
-        "name": "Russia",
-        "citiesList": [
-            {"name": "Moscow", "population": 12678079},
-            {"name": "Saint Petersburg", "population": 5398064},
-            {"name": "Novosibirsk", "population": 1625600},
-            {"name": "Kazan", "population": 1143535},
-            {"name": "Sochi", "population": 343334}
+        name: "Russia",
+        cities: [
+            {name: "Moscow", population: 12678079},
+            {name: "Saint Petersburg", population: 5398064},
+            {name: "Novosibirsk", population: 1625600},
+            {name: "Kazan", population: 1143535},
+            {name: "Sochi", population: 343334}
         ]
     }, {
-        "name": "Germany",
-        "citiesList": [
-            {"name": "Berlin", "population": 3769495},
-            {"name": "Hamburg", "population": 1841179},
-            {"name": "Munich", "population": 1484226}
+        name: "Germany",
+        cities: [
+            {name: "Berlin", population: 3769495},
+            {name: "Hamburg", population: 1841179},
+            {name: "Munich", population: 1484226}
         ]
     }, {
-        "name": "United States of America",
-        "citiesList": [
-            {"name": "Miami", "population": 463347},
-            {"name": "New York", "population": 8622698},
-            {"name": "San Francisco", "population": 884363},
-            {"name": "Seattle", "population": 608660},
-            {"name": "Philadelphia", "population": 1526006}
+        name: "United States of America",
+        cities: [
+            {name: "Miami", population: 463347},
+            {name: "New York", population: 8622698},
+            {name: "San Francisco", population: 884363},
+            {name: "Seattle", population: 608660},
+            {name: "Philadelphia", population: 1526006}
         ]
     }, {
-        "name": "Spain",
-        "citiesList": [
-            {"name": "Barcelona", "population": 1636762},
-            {"name": "Valencia", "population": 801456},
-            {"name": "Malaga", "population": 574654}
+        name: "Spain",
+        cities: [
+            {name: "Barcelona", population: 1636762},
+            {name: "Valencia", population: 801456},
+            {name: "Malaga", population: 574654}
         ]
-    }]
+    }];
 
     console.log("Array of countries objects:");
     console.log(countries);
